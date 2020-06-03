5e41394f8d0487e9c752f0a0b32bc55dd825a0e3
V Rohan Rao <rohanrao.dec11@gmail.com>
2020-06-04 00:37:03 +0530

    Added material theme to Game configurations page

--- front-end/src/components/Config.vue
+++ front-end/src/components/Config.vue
@@ -1,48 +1,146 @@
 <template>
 
-<div class="container-fluid bg-light py-3">
-      <div class="row">
-          <div class="col-md-6 mx-auto">
-              <div class="card card-body">
-                <h1>TEAM</h1>
-                      <form>
-                      <div class="form-group">
-                          <label >FORWARD</label>
-                          <input type="text" v-model="fwd" class="form-control">
-                        </div>
-                      <div class="form-group">
-                          <label >MIDFIELD</label>
-                          <input type="text" class="form-control" v-model="mf">
-                        </div>
-                      <div class="form-group">
-                          <label >DEFENDER</label>
-                          <input type="text" class="form-control" v-model="df">
-                        </div>
-                      <div class="form-group">
-                          <label >GOALKEEPER</label>
-                          <input type="text" class="form-control" v-model="gk">
-                        </div>
-                      <div class="form-group">
-                          <label >MARQUEE Penalty</label>
-                          <input type="text" class="form-control" v-model="mp">
-                      </div>
-                        <button   @click="send" class="btn btn-primary" >SAVE</button>
-                        <button   @click="useDef" class="btn btn-primary float-right">DEFAULT</button>
-                        </form>
-                   </div>
-                 </div>
-            </div>
-      </div>
+  <v-app >
+    <span class="bg"></span>
+    <v-content >
 
+
+        <v-card
+        class="mx-auto overflow-hidden"
+            >
+    <v-app-bar
+      color = "red "
+      
+  
+      
+    >
+    
+      
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+        <v-spacer/>
+      <center><v-toolbar-title><strong> Fantasy Football 2020</strong></v-toolbar-title></center>
+      <v-spacer/>
+    </v-app-bar>
+
+    
+            </v-card>
+
+    
+      <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          active-class="deep-purple--text text--lighten-2"
+        >
+          <router-link tag="span" to='/'>
+          <v-list-item>
+            
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+            
+          </v-list-item>
+          </router-link>
+          
+
+        </v-list-item-group>
+      </v-list>
+      </v-navigation-drawer>
+      <v-container absolute fluid 
+      >
+                            
+
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="light-green darken-1"
+                dark
+                flat
+              >
+              <v-spacer></v-spacer>
+                <v-toolbar-title> Game Configurations </v-toolbar-title>
+                <v-spacer />
+                  
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                    <v-text-field
+                    v-model="gk"
+                    label="Number of goalkeepers"
+                    name="gk"
+                    type="text"
+                  />
+                  
+                  <v-text-field
+                    v-model="mf"
+                    label="Number of mid-fielders"
+                    name="mf"
+                    type="text"
+                  />
+
+                  <v-text-field
+                    v-model="df"
+                    label="Number of defenders"
+                    name="df"
+                    type="text"
+                  /><v-text-field
+                    v-model="fwd"
+                    label="Number of forwards"
+                    name="fwd"
+                    type="text"
+                  /><v-text-field
+                    v-model="mp"
+                    label="Penalty for more than 1 marquee player"
+                    name="mp"
+                    type="text"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn @click ="send" color="primary"> Send </v-btn>
+                <v-btn @click="useDef" color="red darken-4">USE Default</v-btn>
+                
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+
+      </v-container>           
+    </v-content>
+    
+     <Footer/>
+
+  </v-app>
+ 
 </template>
 
 <script>
 import axios from 'axios'
+import Footer from './layout/Footer';
 
 export default {
-  name: 'Config',
+  name: 'Config',components: {
+    Footer
+  },
   data() {
     return {
+      drawer: false,
       mf:'',
       df:'',
       gk:'',
@@ -98,5 +196,16 @@ export default {
 
 <style >
 
-
+.bg {
+    width: 100%;
+    height: 90%;
+    position: absolute;
+    top:0;
+    left: 0;
+    opacity: .4;
+    background: url('../../img/bg1.png' ) no-repeat  ;
+    background-size: cover;
+    background-color: blue;
+    transform: scale(1.1);
+  }
 </style>
---
+++ front-end/src/components/layout/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+<v-card>
+<v-footer
+      :padless= true   absolute >
+      <v-card
+        flat
+        tile
+        width="100%"
+      >
+
+
+        <v-card-text  class="white--text">
+          {{ new Date().getFullYear() }}  <strong>GNU/Linux User's Group, NIT Durgapur</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+    
+        </v-card>
+    </template>
+
+    <script>
+export default {
+  name: "Footer"
+}
+</script>
